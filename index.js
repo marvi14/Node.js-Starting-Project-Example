d53cfc126c405166816c88ae9de064ed53cd71c9
unknown <t_marianos1@MVD-TMARIANOS1.verifone.com>
2016-04-26 12:48:48 -0300

    Fix in port for heroku

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use('/api/v1', require('./api/category_api')(wagner));
 app.use('/api/v1', require('./api/cart_api')(wagner));
 app.use('/api/v1', require('./api/paypal_api')(wagner));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000!');
